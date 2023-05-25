bc97587231f114b80e3e0b72742f9fe63242e302
Sam <robinson.will.sam@gmail.com>
2023-05-25 13:48:20 -0600

    adding resources

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+//variables
+let age = 72;
+//updating variable 
+age = age + 1;
+
+//unary operators: https://www.udemy.com/course/javascript-beginners-complete-tutorial/learn/lecture/16762154#content
+age += 1;
+age *= 2;
+age /= 2;
+age -= 1;
+age--
+age++
+
+//introducing const:https://www.udemy.com/course/javascript-beginners-complete-tutorial/learn/lecture/16762156#content
+const year = 1973;
+const peopleCount = 10;
+
+console.log(year)
+//var: https://www.udemy.com/course/javascript-beginners-complete-tutorial/learn/lecture/16762160#content
+
+
+
+
+
 
